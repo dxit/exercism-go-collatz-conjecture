3272f86da1777e3d0c96621fb1f4c23a5cd2b0c9
Daniele De Matteo <daniele.dematteo@ses.com>
2020-04-07 10:24:53 +0200

    first commit

---
+++ collatz_conjecture.go
@@ -0,0 +1,25 @@
+package collatzconjecture
+
+import "errors"
+
+// CollatzConjecture returns the numbers of steps required to reach 1
+func CollatzConjecture(number int) (int, error) {
+	if number < 1 {
+		return 0, errors.New("unexpected")
+	}
+
+	i := 0
+	for {
+		if number == 1 {
+			break
+		}
+		if number % 2 == 0 {
+			number /= 2
+		} else {
+			number = (number * 3) + 1
+		}
+		i++
+	}
+
+	return i, nil
+}
